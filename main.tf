b72ad183438d549cb040d8d87f37f1539d965c45
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-05-16 20:51:26 -0300

    Documentação

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
+# define as configurações do Terraform em si
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+
+	# define os providers necessários, ou seja, onde a infraestrutura sera implantada
+	required_providers {
+
+		# define como um dos providers um tal provider "aws", poderia ter qualquer nome
+		aws = {
+			source  = "hashicorp/aws"	# fala que o provider é a AWS, para o terraform saber como conversar com ele
+			version = "~> 5.0"			# especifica a versão da API Terraform-AWS
+		}
+	}
 }
 
-# Configure the AWS Provider
+# configurações do provider aws
 provider "aws" {
-  region = "us-east-1"
+	region = "us-east-1"	# define que região da AWS será utilizada
 }
