d59b6adfab87afd73816901df0747040be587aa7
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-05-17 19:28:06 -0300

    Refatoração do script de configuração do wordpress

--- install-wordpress.sh
+++ install-wordpress.sh
@@ -1,23 +1,26 @@
+# atualiza os respositórios dos pacotes e instala as dependências
 apt update
 apt install -y	apache2 \
                	ghostscript \
-               	libapache2-mod-php 
-		mysql-server \
-        	php \
+               	libapache2-mod-php \
+		        mysql-server \
+        	    php \
               	php-bcmath \
-       		php-curl \
-        	php-imagick \
+       		    php-curl \
+        	    php-imagick \
               	php-intl \
-	        php-json \
+	            php-json \
        	        php-mbstring \
                	php-mysql \
-	 	php-xml \
+	 	        php-xml \
                 php-zip
 
+# cria o diretório do wordpress e o baixa nele
 mkdir -p /srv/www
 chown www-data: /srv/www
 curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
 
+# cria o arquivo de configuração do wordpress
 touch /etc/apache2/sites-available/wordpress.conf
 echo "<VirtualHost *:80>
     DocumentRoot /srv/www/wordpress
@@ -33,8 +36,10 @@ echo "<VirtualHost *:80>
     </Directory>
 </VirtualHost>" > /etc/apache2/sites-available/wordpress.conf
 
+# configurações do apache
 a2ensite wordpress
 a2enmod rewrite
 a2dissite 000-default
 
+# reinicia o serviço do apache
 service apache2 reload
